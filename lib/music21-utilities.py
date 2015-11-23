3b8e23024f6d85261a5fc4067852bbbb944d7cd5
musicus <musicus@gmail.com>
2015-11-23 13:36:21 -0500

    One.

---
+++ lib/music21-utilities.py
@@ -0,0 +1,51 @@
+# - *- coding: utf-8 -*-
+#! /usr/local/bin python
+
+import sys, os
+import music21 as mto
+
+def ps_name_to_midi(note_arg):
+	"""
+	Translating a not name with it's octave designation to a midi value
+	(i.e. truncating a note).
+	>>> ps_name_to_midi('C#4')
+	61
+	"""
+	midi_note = mto.pitch.Pitch(str(note_arg))
+	return midi_note.midi
+
+def midi_to_pc_name(midi_arg):
+	"""
+	Convert a midi number to a pitch name. E.g.: 61 => C#.
+	>>> midi_to_pc_name(61)
+	'C#'
+	"""
+	pc_name = mto.pitch.Pitch(int(midi_arg))
+	return pc_name.name
+
+def midi_to_ps_name(midi_arg):
+	"""
+	Convert a midi number to a pitch space name. E.g.: 61 => C#4.
+	>>> midi_to_ps_name(61)
+	'C#4'
+	"""
+	ps_name = mto.pitch.Pitch(int(midi_arg))
+	return ps_name.nameWithOctave
+
+def bang_test(bang='bang'):
+	"""
+	Receive and evaluate a bang.
+	>>> bang_test('bang')
+	'bang'
+	"""
+	return bang
+
+def main():
+	"""
+	Using main only for doctest.
+	"""
+	import doctest
+	doctest.testmod()
+
+if __name__ == '__main__':
+	main()
