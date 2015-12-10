35a64a3610a335d02a759991533b25b8f8584d88
musicus <musicus@gmail.com>
2015-12-10 18:18:30 -0500

    Staged Vertical, Horizontal, and NGram indexers for class development.

---
+++ lib/VIS-HInt.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+'''
+# ----- VIS-HInt.py -------------------------------------------------- #
+
+Author: Reiner Kramer
+Email:  reiner@music.org
+Date:   12.10.2015
+
+The following class encapsulates the VIS Framework's Horizontal 
+Interval indexer class.
+
+# -------------------------------------------------------------------- #
+
+
+
+# ----- END VIS-HInt.py ---------------------------------------------- #
---
+++ lib/VIS-VInt.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+'''
+# ----- VIS-VInt.py -------------------------------------------------- #
+
+Author: Reiner Kramer
+Email:  reiner@music.org
+Date:   12.10.2015
+
+The following class encapsulates the vis-frameworks Vertical Interval 
+indexer class.
+
+# -------------------------------------------------------------------- #
+
+
+
+# ----- END VIS-VInt.py ---------------------------------------------- #
