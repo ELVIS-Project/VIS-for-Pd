a4c1406bd7f1443f95ebd8628d6fd7d65d925ad7
musicus <musicus@gmail.com>
2016-04-06 16:54:09 -0400

    Updates to the NoteRestIndexer. Now Users can also just view data slices.

--- lib/VIS-Tools.py
+++ lib/VIS-Tools.py
@@ -7,7 +7,7 @@ passed to another in Pd.
 
 Author:		Reiner Kramer	
 Email:		reiner@music.org
-Updated:	04.05.2016
+Updated:	04.06.2016
 
 Todo: Lots ... Converting or dumping DataFrame into CSV?
 
@@ -24,7 +24,7 @@ try:
                 sys.version_info[1],
                 sys.version_info[2]))
 except:
-	print("Failed")
+		print("Failed")
         
 class NoteRestIndexing(pyext._class):
 
@@ -33,12 +33,12 @@ class NoteRestIndexing(pyext._class):
 	"""
 
 	# How many inlets and outlets.
-	_inlets = 4
+	_inlets = 5
 	_outlets = 1
 
 	# Init function.
 	def __init__(self,mto_score=0,ind_score=0,events=5,
-		direction='beginning'):
+		direction='beginning',slice_start=0,slice_end=3):
 		"""
 		Init function for storing variables used in this class.
 		"""
@@ -46,6 +46,8 @@ class NoteRestIndexing(pyext._class):
 		self.ind_score = ind_score
 		self.events = events
 		self.direction = direction
+		self.slice_start = slice_start
+		self.slice_end = slice_end
 
 	# Handling inlets.
 	def _anything_1(self,symbolic_score):
@@ -83,9 +85,10 @@ class NoteRestIndexing(pyext._class):
 		Determines how many events are to be shown.
 		"""
 		if(self.mto_score == 0):
-			print("Please load a symbolic score first.")
+			self._msg_missing_score()
 		else:
 			self.events = events
+			# The beginning or the end of the DataFrame
 			self._heads_or_tails()
 
 	def _anything_4(self,direction):
@@ -94,12 +97,21 @@ class NoteRestIndexing(pyext._class):
 		the end.
 		"""
 		if(self.mto_score == 0):
-			print("Please load a symbolic score first.")
+			self._msg_missing_score()
 		else:
 			self.direction = str(direction)
 			self._heads_or_tails()
 
-	# Local method, to comply with DRY.
+	def _anything_5(self,slice_start,slice_end):
+		"""
+		Picks a slice from a given DataFrame.
+		"""
+		if(self.mto_score == 0):
+			self._msg_missing_score()
+		else:
+			print(self.ind_score.iloc[slice_start:slice_end])
+
+	# Local methods: complying with DRY.
 	def _heads_or_tails(self):
 		"""
 		Helper method to determine whether to count from the beginning
@@ -111,5 +123,11 @@ class NoteRestIndexing(pyext._class):
 		else:
 			print(self.ind_score.head(self.events))
 			#self._outlet(1,self.ind_score.head(self.events).to_csv())
+
+	def _msg_missing_score(self):
+		"""
+		Prints a message to the Pd window that a score has not been loaded.
+		"""
+		print("Please load a symbolic score first.")
 
 # ----- END VIS-Tools.py --------------------------------------------- #
